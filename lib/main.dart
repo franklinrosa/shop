950748931484067f71db27572d85063f2a220fc9
Franklin Rosa <franklingarciadarosa@gmail.com>
2025-07-03 20:45:51 -0300

    3 03/07/25

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
 import 'package:shop/models/order_list.dart';
-import 'package:shop/models/product.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
 import 'package:shop/pages/orders_page.dart';
@@ -51,7 +50,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.cart: (ctx) => const CartPage(),
           AppRoutes.orders: (ctx) => const OrdersPage(),
           AppRoutes.products: (ctx) => const ProductsPage(),
-          AppRoutes.product_from: (ctx) => const ProductFormPage()
+          AppRoutes.productFrom: (ctx) => const ProductFormPage()
         },
         debugShowCheckedModeBanner: false,
       ),
